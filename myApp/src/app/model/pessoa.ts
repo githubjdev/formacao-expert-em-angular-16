2bec06f17affccd9f389cf4d3b2846d45cb1c86c
afgale <alex.fernando.egidio@gmail.com>
2024-04-17 16:30:46 -0300

    Formação Expert em Angular 16

---
+++ myApp/src/app/model/pessoa.ts
@@ -0,0 +1,16 @@
+import { PessoaJuridica } from "./pessoa-juridica";
+
+export class  Pessoa {
+
+    
+    id?: Number;
+    nome?: String;
+    email?: String;
+    telefone?:String;
+    tipoPessoa?: String;
+    empresa?: PessoaJuridica;
+
+    constructor(){       
+    }
+   
+}
